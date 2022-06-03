ddea3b2079c2de3d53aca76f5445501ba928bb9e
GitBen <dowonblee@gmail.com>
2022-06-02 18:06:19 -0700

    began on one of the APIs in JS

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+// API request for USDA food list
+$.getJSON("https://api.nal.usda.gov/fdc/v1/foods/list?api_key=nZxmwjSMlpgFxlnvqEMSLAhOnpHCFmRxENBsiGIA", function(data){
+
+    console.log(data);
+
+});
