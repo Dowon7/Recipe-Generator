e45e5a3fbfc1280702ee80fa7d33f57c51e7caac
GitBen <dowonblee@gmail.com>
2022-06-02 21:59:34 -0700

    Added layout, fixed navBar, implemented Modal, switched to MD Bootstrap Framework, API call for USDA

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,13 @@
-// API request for USDA food list
-$.getJSON("https://api.nal.usda.gov/fdc/v1/foods/list?api_key=nZxmwjSMlpgFxlnvqEMSLAhOnpHCFmRxENBsiGIA", function(data){
 
-    console.log(data);
+function fetchRecipes() {
+    // API request for USDA food list
+    $.getJSON("https://api.nal.usda.gov/fdc/v1/foods/list?api_key=nZxmwjSMlpgFxlnvqEMSLAhOnpHCFmRxENBsiGIA", function(data){
+    
+        console.log(data);
+    
+    });
 
-});
+}
+
+const ingredientSearchButton = document.getElementById("ingredientSearchButton");
+ingredientSearchButton.addEventListener("click", fetchRecipes);
