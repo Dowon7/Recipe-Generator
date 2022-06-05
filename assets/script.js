08a0dc9481b7cec4f83d8363f7608f826f9cdeb6
leono3@github.com <leono3@github.com>
2022-06-04 17:09:45 -0700

    Add and remove ingredients

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,44 @@
+let ingredientCount = 0;
+
+
+function getIngredient() {
+	const ingredientEls = $(".ingredient-input");
+	console.log(ingredients)
+	//single responsibility
+    // block scope 
+    const ingredients = ingredientEls.map(ingredientEl => console.log(ingredientEl));
+}
+
+function addIngredient() {
+    let newIngredient = document.getElementById("ingredient-input").value;
+    console.log(newIngredient);
+    const ingredientEl = $("<li>").addClass("list-group-item list-group-item-action");
+    const ingridient = $("<span>");
+    ingridient.text(newIngredient);
+    const delButton = $("<button>").addClass("btn-close");
+    delButton.attr("aria-label","Close");
+    delButton.attr("item-count", ingredientCount);
+    delButton.click(removeIngredient);
+    const itemWrapper = $("<div>").addClass("list-item-wrapper");
+    itemWrapper.append(ingridient);
+    itemWrapper.append(delButton);
+    const listItem = $("<li>").addClass(`list-group-item list-group-item-action ${ingredientCount}`);
+    listItem.append(itemWrapper);
+    const ingredientList = $(".ingredient-list");
+    ingredientList.append(listItem);
+    document.getElementById("ingredient-input").value = "";
+    ingredientCount +=1;
+    return delButton;
+    
+}
+
+function removeIngredient(event){
+    let number = event.target;
+    console.log(number.getAttribute("item-count"));
+    console.log(event.target);
+    const removedItem = $(`.${number.getAttribute("item-count")}`);
+    removedItem.remove();
+}
 
 function fetchRecipes() {
     // API request for USDA food list
