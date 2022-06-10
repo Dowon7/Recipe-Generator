1c5aad341c4b9b5932b26241828da2d9eee8b9fc
BoiledLettuce <tktjoshuahenson@yahoo.com>
2022-06-10 15:35:29 -0700

    returned to working scripts

--- assets/script.js
+++ assets/script.js
@@ -2,17 +2,13 @@ let ingredientCount = 0;
 let ingredientArray = [];
 let recipeList = [];
 
-function fetchRecipes() {
-    // API request for USDA food list
-    $.getJSON("https://api.nal.usda.gov/fdc/v1/foods/list?api_key=nZxmwjSMlpgFxlnvqEMSLAhOnpHCFmRxENBsiGIA", function (data) {
-
-        console.log(data);
-
-        for (var i = 0; i < data.length; i++) { // for loop to test parsing info *.description*
-            console.log(data[i].description);
-        }
-
-    });
+// function getIngredient() {
+// 	const ingredientEls = $(".ingredient-input");
+// 	console.log(ingredients)
+// 	//single responsibility
+//     // block scope 
+//     const ingredients = ingredientEls.map(ingredientEl => console.log(ingredientEl));
+// }
 
 document.getElementById('ingredient-input').addEventListener("keypress", function(event) {
     // If the user presses the "Enter" key on the keyboard
@@ -59,89 +55,14 @@ function removeIngredient(event){
     console.log(ingredientArray);
 }
 
-function fetchSpoon() {
-
-    $.getJSON("https://api.spoonacular.com/recipes/complexSearch?apiKey=f1ff032dbf3e458ea914a1c136a78dcc", function (data) {
-
-        console.log(data);
-
-    });
-}
-
-function randomRecipes() {
-    $.getJSON("https://api.spoonacular.com/recipes/random?apiKey=f1ff032dbf3e458ea914a1c136a78dcc", function (data) {
-
-        console.log(data);
-
-    })
-}
-
-
-let ingredientCount = 0;
-let ingredientArray = [];
-let recipeList = [];
-
-// function getIngredient() {
-// 	const ingredientEls = $(".ingredient-input");
-// 	console.log(ingredients)
-// 	//single responsibility
-//     // block scope 
-//     const ingredients = ingredientEls.map(ingredientEl => console.log(ingredientEl));
-// }
-
-document.getElementById('ingredient-input').addEventListener("keypress", function (event) {
-    // If the user presses the "Enter" key on the keyboard
-    if (event.key === "Enter") {
-        // Cancel the default action, if needed
-        event.preventDefault();
-        // Trigger the button element with a click
-        document.getElementById("add-button").click();
-    }
-});
-
-function addIngredient() {
-    let newIngredient = document.getElementById("ingredient-input").value;
-    console.log(newIngredient);
-    ingredientArray[ingredientCount] = newIngredient;
-    console.log(ingredientArray);
-    // const ingredientEl = $("<li>").addClass("list-group-item list-group-item-action");
-    const ingridient = $("<span>");
-    ingridient.text(newIngredient);
-    const delButton = $("<button>").addClass("btn-close");
-    delButton.attr("aria-label", "Close");
-    delButton.attr("item-count", ingredientCount);
-    delButton.click(removeIngredient);
-    const itemWrapper = $("<div>").addClass("list-item-wrapper");
-    itemWrapper.append(ingridient);
-    itemWrapper.append(delButton);
-    const listItem = $("<li>").addClass(`list-group-item list-group-item-action ${ingredientCount}`);
-    listItem.append(itemWrapper);
-    const ingredientList = $(".ingredient-list");
-    ingredientList.append(listItem);
-    document.getElementById("ingredient-input").value = "";
-    ingredientCount += 1;
-    return delButton;
-
-}
-
-function removeIngredient(event) {
-    let number = event.target;
-    console.log(number.getAttribute("item-count"));
-    console.log(event.target);
-    const removedItem = $(`.${number.getAttribute("item-count")}`);
-    removedItem.remove();
-    ingredientArray[number.getAttribute("item-count")] = null;
-    console.log(ingredientArray);
-}
-
-function recipeFinder() {
+function recipeFinder(){
     const filtered = ingredientArray.filter(function (el) {
         return el != null;
     });
     searchRecipe(filtered.join(", "));
 }
 
-function searchRecipe(keyWords) {
+function searchRecipe(keyWords){
     const url = "https://spoonacular-recipe-food-nutrition-v1.p.rapidapi.com/recipes/findByIngredients?ingredients=" + keyWords + "&number=10"
     const settings = {
         "async": true,
@@ -154,14 +75,13 @@ function searchRecipe(keyWords) {
         }
     };
     $.ajax(settings).done(function (response) {
-        console.log(response);
-        recipeList = response;
-        displayRecipe(recipeList);
-        resetFind();
-    })
-}
+	console.log(response);
+    recipeList = response;
+    displayRecipe(recipeList);
+    resetFind();
+})}
 
-function displayRecipe(recipeList) {
+function displayRecipe(recipeList){
     var recipe1 = $(".card1-img-top");
     recipe1.attr("src", recipeList[0].image);
     var label1 = $(".card-title1");
@@ -176,10 +96,9 @@ function displayRecipe(recipeList) {
     label3.text(recipeList[2].title);
 }
 
-function resetFind() {
+function resetFind(){
     $(".list-group-item").remove();
     ingredientCount = 0;
     ingredientArray = [];
     recipeList = [];
 }
-
