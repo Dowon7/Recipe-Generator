2b53377225fbf3c8a737bd1390bc0f65f26bc17e
GitBen <dowonblee@gmail.com>
2022-06-14 04:43:37 -0700

    merging Leo's pushes manually

--- assets/script.js
+++ assets/script.js
@@ -103,14 +103,15 @@ function resetFind(){
     recipeList = [];
 }
 
-// function fetchRecipes() {
-//     // API request for USDA food list
-//     $.getJSON("https://api.nal.usda.gov/fdc/v1/foods/list?api_key=nZxmwjSMlpgFxlnvqEMSLAhOnpHCFmRxENBsiGIA", function(data){
+// USDA API
+function fetchRecipes() {
+    // API request for USDA food list
+    $.getJSON("https://api.nal.usda.gov/fdc/v1/foods/list?api_key=nZxmwjSMlpgFxlnvqEMSLAhOnpHCFmRxENBsiGIA", function(data){
     
-//         console.log(data);
+        console.log(data);
     
-//     });
+    });
 
-// // }
+}
 
 // Testing my new branch hihi 
